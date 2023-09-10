8f706a17fbef7cc03c4e77bccea3ac1bd663054e
eftieell <128193706+eftieell@users.noreply.github.com>
2023-09-10 08:47:50 -0600

    Update text_demo.py
    Added parameter to show command

--- src/programs/dudraw_quickstart/text_demo.py
+++ src/programs/dudraw_quickstart/text_demo.py
@@ -13,4 +13,4 @@ dudraw.text(0.5, 0.6, "Times New Roman 12 point")
 dudraw.set_font_family("Arial")
 dudraw.set_font_size(6)
 dudraw.text(0.5, 0.8, "Arial 6 point")
-dudraw.show()
+dudraw.show(10000) # display the result for 10 seconds
